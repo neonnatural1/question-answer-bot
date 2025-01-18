094356741924a3d7828218d7a33168388460b48f
Olcay Şar <54412868+neonnatural1@users.noreply.github.com>
2025-01-18 22:05:44 +0000

    made a simple question answering bot

---
+++ qa_bot_app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from transformers import pipeline
+
+answerbot = pipeline("question-answering")
+
+st.title("🎈 QA Bot")
+st.write(
+    "This is a basic question answering bot. You have to give the context for it to guess the answer. For example:  \nQuestion: What color is apple?  \nContext: Apple is red.  \nAnswer: red"   
+)
+# Add a text input box
+user_input1 = st.text_input("Enter some question:")
+user_input2 = st.text_input("Enter the context:")
+
+# Button to trigger action
+if st.button("Submit"):
+    answerraw = answerbot(question=f"{user_input1}", context=f"{user_input2}")
+    answer = answerraw["answer"]
+    st.write(f"Answer: {answer}")
+    certainty = answerraw["score"]
+    st.write(f"Certainty:{certainty:.2f}")
